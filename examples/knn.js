b7b68916497fbecc1e2892a2e6a474a4601893ef
sunchenguang <809200299@qq.com>
2017-03-12 19:28:06 +0800

    update: 修改knn.js for demo

--- examples/knn.js
+++ examples/knn.js
@@ -1,33 +1,60 @@
 /**
  * Created by joonkukang on 2014. 1. 19..
  */
-var ml = require('../lib/machine_learning');
+let ml = require('../lib/machine_learning');
 
-var data = [[1,0,1,0,1,1,1,0,0,0,0,0,1,0],
-            [1,1,1,1,1,1,1,0,0,0,0,0,1,0],
-            [1,1,1,0,1,1,1,0,1,0,0,0,1,0],
-            [1,0,1,1,1,1,1,1,0,0,0,0,1,0],
-            [1,1,1,1,1,1,1,0,0,0,0,0,1,1],
-            [0,0,1,0,0,1,0,0,1,0,1,1,1,0],
-            [0,0,0,0,0,0,1,1,1,0,1,1,1,0],
-            [0,0,0,0,0,1,1,1,0,1,0,1,1,0],
-            [0,0,1,0,1,0,1,1,1,1,0,1,1,1],
-            [0,0,0,0,0,0,1,1,1,1,1,1,1,1],
-            [1,0,1,0,0,1,1,1,1,1,0,0,1,0]
-           ];
+let data = [
+    [1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0],
+    [1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0],
+    [0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0]
+];
 
-var result = [23,12,23,23,45,70,123,73,146,158,64];
+let result = [23, 12, 23, 23, 45, 70, 123, 73, 146, 158, 64];
 
-var knn = new ml.KNN({
-    data : data,
-    result : result
+let knn = new ml.KNN({
+    data: data,
+    result: result
 });
 
-var y = knn.predict({
-    x : [0,0,0,0,0,0,0,1,1,1,1,1,1,1],
-    k : 3,
-    weightf : {type : 'gaussian', sigma : 10.0},
-    distance : {type : 'euclidean'}
+let y = knn.predict({
+    x: [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+    k: 3,
+    weightf: {type: 'gaussian', sigma: 10.0},
+    distance: {type: 'euclidean'}
 });
 
-console.log(y);
+console.log(`y value predict: ${y}`);
+
+let data_film = [
+    [3, 104],
+    [4, 100],
+    [1, 81],
+    [101, 10],
+    [99, 3],
+    [98, 5]
+];
+
+// let result_film = ['喜剧片', '喜剧片', '喜剧片', '动作片', '动作片', '动作片'];
+let result_film = [0, 0, 0, 1, 1, 1];
+
+let knn_film = new ml.KNN({
+    data: data_film,
+    result: result_film
+});
+
+let category = knn_film.predict({
+    x: [18, 90],
+    k: 3,
+    weightf: {type: 'gaussian', sigma: 10.0},
+    distance: {type: 'euclidean'}
+});
+
+console.log(`movie type predict: ${category}`)
